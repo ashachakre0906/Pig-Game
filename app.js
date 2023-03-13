839eff2e1d9e725d92ab40f44edc42216be82153
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-13 13:58:30 -0700

    adding event listener to the roll dice button,generating random dice roll and removing hidden class of the image

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 //Selecting elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+const buttonNew = document.querySelector(".btn btn--new");
+const buttonRoll = document.querySelector(".btn btn--roll");
+const buttonHold = document.querySelector(".btn btn--hold");
 // Setting the text content of the score to 0;
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -11,3 +14,21 @@ score1El.textContent = 0;
 
 let diceEl = document.querySelector(".dice");
 diceEl.classList.add("hide");
+//Adding event listener to the button
+//event name >>click
+//click >>event handler function
+buttonRoll.addEventListener("click", function () {
+  console.log("button clicked");
+  //we need to start by generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  //Math.random which creates the number between 0 and 1
+  //Multiply that by 6, we will get a number between 0 and 6 but not 6 so add 1 to it
+  //truncate all of this by doing Math.trunc () so we removed the decimal part.
+  //Basically we pass the result of `Math.random() * 6`into Math.trunc so this will then give us a number between 0 and 5 so we just add one to elevate that to one to six.
+  //Remove the hidden class
+  diceEl.classList.remove("hide");
+  //Check if the rolled dice is 1 >>Switch to the next player
+   //Dice will be number between 1 and 6
+   //we have to display one of the images
+   //we can manipulate this source(src) attribute from our JS 
+});
