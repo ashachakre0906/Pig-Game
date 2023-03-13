d9ad2f88faea8f4a81d6c7aee065df4c7b2fb93e
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-13 13:10:08 -0700

    setting the text content of the intial score to 0 and hiding the dice element

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 "use strict";
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+//Selecting elements
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+// Setting the text content of the score to 0;
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+//Hiding the dice element
+//grab the the dice element and Create the hide class
+
+let diceEl = document.querySelector(".dice");
+diceEl.classList.add("hide");
