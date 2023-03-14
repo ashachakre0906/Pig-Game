877fec7c736cbe4bfe46c1e021c38b16c88d3b4f
ashachakre0906 <chourpagar.asha@gmail.com>
2023-03-14 12:30:11 -0700

    using toggle method adding and removing class chnaging teh color and displaying who is a active player

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const buttonRoll = document.querySelector(".btn--roll");
 const buttonHold = document.querySelector(".btn--hold");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector(".player--1");
 
 // Setting the text content of the score to 0;
 score0El.textContent = 0;
@@ -54,5 +56,8 @@ buttonRoll.addEventListener("click", function () {
     //When we switch the player change the value to 0 to 1 or 1 to 1 using ternary operator.
       activePlayer = activePlayer === 0 ? 1 : 0;//reassigning the value to active player switching value between 0 and 1.
       currentScore = 0;
+      player0El.classList.toggle("player--active");
+      player1El.classList.toggle("player--active");
+
   }
 });
